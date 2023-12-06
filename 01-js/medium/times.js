8af22a8cbfad1282fac31252984ad6affc5ce8bc
debabratabar <debabrata.bar1999@gmail.com>
2023-12-07 03:40:02 +0530

    time practice

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,26 +8,28 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime() {
-    let date = new Date()
-    // console.log(date);
-    // console.log("current Time:");
-    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`);
+
+// let dateTime = new Date()
+
+// let initial_time= dateTime.getTime()
+
+// let sum =0
+// for(let i=0;i<10000000000;i++){
+//     sum+=i
+// }
+
+// let end_time = dateTime.getTime()
+
+// let total_time_taken = end_time-initial_time;
+
+// console.log(`Total time taken ${total_time_taken}`);
+
+// creating stopwatch  in terminal
+
+
+function printTime() {
+    let time= new Date()
+    console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
 }
 
-console.log("Start Time");
-calculateTime()
-
-// let time = new Date().getHours();
-// console.log(time);
-let sum=0
-for (let i=1;i<1000000000;i++ ){
-    sum+=i;
-}
-
-console.log("End Time");
-calculateTime()
-
-
-
-
+setInterval(printTime, 1000)
